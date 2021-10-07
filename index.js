d5420f98289151324c4ee2912552f7b724df2e15
khushi <khushidang05@gmail.com>
2021-10-07 13:38:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to express</h1>')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Cntact us</h1>')
+})
+
+
+
+app.listen(8200, () => {
+    console.log("Server Connected");
+})
---
+++ indexHTTP.js
@@ -0,0 +1,18 @@
+// const http = require('http')
+
+// http.createServer((req, res) => {
+//     console.log("Server Connected");
+
+//     if (req.url == '/') {
+//         res.write('<h1>Welcome to nodejs</h1>')
+//         res.end()
+//     } else if (req.url == '/contact') {
+//         res.write('<h1>Contact US</h1>')
+//         res.end()
+//     }
+
+
+//     // console.log(req);
+//     // console.log("Server Disconected");
+
+// }).listen(8000)
